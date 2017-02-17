738fdfcc732bd24235df2aa3c89758c2edc85633
Colin Farquhar <cifarquhar@gmail.com>
2017-02-17 11:05:52 +0000

    add update method to customer class

--- models/customer.rb
+++ models/customer.rb
@@ -27,6 +27,12 @@ class Customer
     self.get_many(sql)
   end
 
+  def update()
+    sql = "UPDATE customers SET (name,funds) = ('#{@name}', #{@funds}) WHERE id = #{@id};"
+    SqlRunner.run(sql)
+  end
+
+
 
 
 end
