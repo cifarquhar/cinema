c38a1dc19318467b7440308bd3349ccf54e6dbd4
Colin Farquhar <cifarquhar@gmail.com>
2017-02-17 10:30:34 +0000

    add cinema.sql file

---
+++ db/cinema.sql
@@ -0,0 +1,20 @@
+DROP TABLE tickets;
+DROP TABLE customers;
+DROP TABLE films;
+
+CREATE TABLE customers(
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255),
+  funds INT2
+);
+
+CREATE TABLE films(
+  id SERIAL4 PRIMARY KEY,
+  title VARCHAR(255),
+  funds INT2
+);
+
+CREATE TABLE tickets(
+  customer_id INT4 REFERENCES customers(id),
+  film_id INT4 REFERENCES films(id)
+);
